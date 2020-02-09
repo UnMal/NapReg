e0d002659aa8b41041773c27e1e83ea0aedc6814
UnMal <lordvonhymannovitch@gmail.com>
2020-02-09 16:32:30 +0100

    projet android

---
+++ app/src/main/java/com/ngabeket/napreg/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ngabeket.napreg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState1);
+        setContentView(R.layout.activity_main);
+    }
+}
